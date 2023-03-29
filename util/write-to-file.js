49788c221092dd3aa17297ed39034ecb07b28547
Nikolay Azaryan <n.a.azaryan@outlook.com>
2023-03-29 16:22:10 +0300

    created function to save data as JSON on movies.json

---
+++ util/write-to-file.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (data) => {
+    console.log("the data to write in file :", data);
+    try {
+        fs.writeFileSync(
+        path.join(__dirname, "..", "data", "movies.json"), 
+        JSON.stringify(data), 
+        "utf-8"
+        );
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
